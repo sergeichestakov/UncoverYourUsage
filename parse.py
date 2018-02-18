deba9072f3f0af0fd717acb5fcd72b7574cb7ccb
Sergei Chestakov <schestakov@ucdavis.edu>
2018-02-17 23:39:44 -0800

    decrease layers in model and format output

--- parse.py
+++ parse.py
@@ -10,7 +10,7 @@ from keras.wrappers.scikit_learn import KerasClassifier
 energy_data = './data/recs2009_public.csv'
 
 INPUT = ['YEARMADE', 'BEDROOMS', 'SIZRFRI1', 'TVCOLOR', 'TEMPHOME'] #Input Vars
-OUTPUT = ['TOTALBTU'] #Total Energy consumed in BTU
+OUTPUT = ['KWH'] #Total Energy consumed in BTU
 TRAIN_ENTRIES = 9000 #Remainder is test (12083 Total)
 df = pd.read_csv(energy_data, low_memory=False)
 
@@ -24,23 +24,36 @@ def normalize(rawpoints, high=1.0, low=0.0):
     mins = np.min(rawpoints, axis=0)
     maxs = np.max(rawpoints, axis=0)
     rng = maxs - mins
-    return high - (((high - low) * (maxs - rawpoints)) / rng)
+    return (rawpoints - mins) / (maxs - mins)
+
+def denormalize(original, target):
+    mins = np.min(original, axis=0)
+    maxs = np.max(original, axis=0)
+
+    return target * (maxs - mins) + mins
 
 def createModel(optimizer="adam"):
     model = Sequential()
-    model.add(Dense(60, input_dim=5, activation='relu'))
-    model.add(Dropout(0.02))
-    model.add(Dense(32,activation="softmax"))
-    model.add(Dropout(0.02))
-    model.add(Dense(10,activation="relu"))
+    model.add(Dense(3, input_dim=5, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
 
     model.compile(loss='mean_squared_error', optimizer=optimizer)
-               
+
     return model
 
 trainXScaled = normalize(trainX)
 trainYScaled = normalize(trainY)
 
+testXScaled = normalize(testX)
+testYScaled = normalize(testY)
+
 model = createModel()
-model.fit(trainXScaled, trainYScaled, batch_size=3, epochs=10,verbose=1)
+model.fit(trainXScaled, trainYScaled, batch_size=25, epochs=3,verbose=1)
+
+results = model.predict(testXScaled, verbose=1)
+for index in range(len(testY)):
+    output = testY.values[index][0]
+    prediction = denormalize(testY, results[index]).item()
+    print('Index ', index)
+    print('OUTPUT: ', output)
+    print('PREDICTION: ', prediction)
