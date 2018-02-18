dbc10a25037f885a4c6855c6f90b3879b30679fa
Sergei Chestakov <schestakov@ucdavis.edu>
2018-02-17 18:06:01 -0800

    partition data and begin training model

--- parse.py
+++ parse.py
@@ -2,42 +2,41 @@ import keras
 import os
 import sys
 import pandas as pd
-
-energy_data = './data/recs2009_public.csv'
-
-df = pd.read_csv(energy_data, low_memory=False)
-print(df)
-from keras.wrappers.scikit_learn import KerasClassifier
-
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
+from keras.wrappers.scikit_learn import KerasClassifier
 
-'''
-trainX = df[df.columns[0:14][0:1000]]
-testX = df[df.columns[0:14][1000:]]
+energy_data = './data/recs2009_public.csv'
 
-trainY = df[df.columns[15]][0:1000]
-testY = df[df.columns[15]][1000:]
+INPUT = ['YEARMADE', 'BEDROOMS', 'SIZRFRI1', 'TVCOLOR', 'TEMPHOME']
+OUTPUT = 'TOTALBTU'
+TRAIN_ENTRIES = 9000 #Remainder is test (12083 Total)
+df = pd.read_csv(energy_data, low_memory=False)
+print(df)
 
+trainX = df[INPUT][0:TRAIN_ENTRIES]
+testX = df[INPUT][TRAIN_ENTRIES:]
+print(trainX)
+print(testX)
+trainY = df[OUTPUT][0:TRAIN_ENTRIES]
+testY = df[OUTPUT][TRAIN_ENTRIES:]
+print(trainY)
+print(testY)
 
-
-def modelx(optimizer="adam"):
+def createModel(optimizer="adam"):
     model = Sequential()
-    model.add(Dense(64, input_dim=14, activation='relu'))
+    model.add(Dense(64, input_dim=5, activation='relu'))
     model.add(Dropout(0.02))
     model.add(Dense(32,activation="softmax"))
     model.add(Dropout(0.02))
     model.add(Dense(10,activation="relu"))
     model.add(Dense(1, activation='sigmoid'))
 
-    model.compile(loss='binary_crossentropy',
+    model.compile(loss='mse',
                   optimizer=optimizer,
                   metrics=['accuracy'])
     return model
-    
-from sklearn.model_selection import GridSearchCV
-parameters = {'optimizer'😞'sgd', 'adam','adagrad','rmsprop','adadelta')}
-clf = GridSearchCV(modelx,parameters)
-modelx.fit(np.array(trainX), trainY2, batch_size=25, epochs=100,verbose=1)
-'''
+
+model = createModel()
+model.fit(np.array(trainX), trainY, batch_size=10, epochs=100,verbose=1)
