7570a30734f2924780083dcc1fd92a7b55fbd244
Sergei Chestakov <schestakov@ucdavis.edu>
2018-02-17 19:19:04 -0800

    complete model training by normalizing input

--- parse.py
+++ parse.py
@@ -9,34 +9,38 @@ from keras.wrappers.scikit_learn import KerasClassifier
 
 energy_data = './data/recs2009_public.csv'
 
-INPUT = ['YEARMADE', 'BEDROOMS', 'SIZRFRI1', 'TVCOLOR', 'TEMPHOME']
-OUTPUT = 'TOTALBTU'
+INPUT = ['YEARMADE', 'BEDROOMS', 'SIZRFRI1', 'TVCOLOR', 'TEMPHOME'] #Input Vars
+OUTPUT = ['TOTALBTU'] #Total Energy consumed in BTU
 TRAIN_ENTRIES = 9000 #Remainder is test (12083 Total)
 df = pd.read_csv(energy_data, low_memory=False)
-print(df)
 
 trainX = df[INPUT][0:TRAIN_ENTRIES]
 testX = df[INPUT][TRAIN_ENTRIES:]
-print(trainX)
-print(testX)
+
 trainY = df[OUTPUT][0:TRAIN_ENTRIES]
 testY = df[OUTPUT][TRAIN_ENTRIES:]
-print(trainY)
-print(testY)
+
+def normalize(rawpoints, high=1.0, low=0.0):
+    mins = np.min(rawpoints, axis=0)
+    maxs = np.max(rawpoints, axis=0)
+    rng = maxs - mins
+    return high - (((high - low) * (maxs - rawpoints)) / rng)
 
 def createModel(optimizer="adam"):
     model = Sequential()
-    model.add(Dense(64, input_dim=5, activation='relu'))
+    model.add(Dense(60, input_dim=5, activation='relu'))
     model.add(Dropout(0.02))
     model.add(Dense(32,activation="softmax"))
     model.add(Dropout(0.02))
     model.add(Dense(10,activation="relu"))
     model.add(Dense(1, activation='sigmoid'))
 
-    model.compile(loss='mse',
-                  optimizer=optimizer,
-                  metrics=['accuracy'])
+    model.compile(loss='mean_squared_error', optimizer=optimizer)
+               
     return model
 
+trainXScaled = normalize(trainX)
+trainYScaled = normalize(trainY)
+
 model = createModel()
-model.fit(np.array(trainX), trainY, batch_size=10, epochs=100,verbose=1)
+model.fit(trainXScaled, trainYScaled, batch_size=3, epochs=10,verbose=1)
