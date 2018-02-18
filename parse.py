06c4ac1955f38d293bd5d8542640910571c6592b
Sergei Chestakov <schestakov@ucdavis.edu>
2018-02-18 01:34:37 -0800

    change model configs and add comments

--- parse.py
+++ parse.py
@@ -20,40 +20,55 @@ testX = df[INPUT][TRAIN_ENTRIES:]
 trainY = df[OUTPUT][0:TRAIN_ENTRIES]
 testY = df[OUTPUT][TRAIN_ENTRIES:]
 
+#Normalize all values in array to between 0 and 1
 def normalize(rawpoints, high=1.0, low=0.0):
     mins = np.min(rawpoints, axis=0)
     maxs = np.max(rawpoints, axis=0)
     rng = maxs - mins
     return (rawpoints - mins) / (maxs - mins)
 
+#Return normalized values back to original
 def denormalize(original, target):
     mins = np.min(original, axis=0)
     maxs = np.max(original, axis=0)
 
     return target * (maxs - mins) + mins
 
-def createModel(optimizer="adam"):
+#Construct Neural Network model
+def createModel(optimizer='sgd'):
     model = Sequential()
-    model.add(Dense(3, input_dim=5, activation='relu'))
+
+    model.add(Dense(15, input_dim=5, activation='relu'))
+    model.add(Dense(12))
+    model.add(Dense(7, activation='softmax'))
+    model.add(Dense(3))
     model.add(Dense(1, activation='sigmoid'))
 
     model.compile(loss='mean_squared_error', optimizer=optimizer)
 
     return model
 
+#Normalize data
 trainXScaled = normalize(trainX)
 trainYScaled = normalize(trainY)
 
 testXScaled = normalize(testX)
 testYScaled = normalize(testY)
 
+#Initialize and train model
 model = createModel()
-model.fit(trainXScaled, trainYScaled, batch_size=25, epochs=3,verbose=1)
+model.fit(trainXScaled, trainYScaled, batch_size=25, epochs=3, verbose=1)
 
+#Predict and output results
 results = model.predict(testXScaled, verbose=1)
-for index in range(len(testY)):
+length = len(testY)
+sum = 0
+for index in range(length):
     output = testY.values[index][0]
     prediction = denormalize(testY, results[index]).item()
-    print('Index ', index)
+    sum += abs(output - prediction)
     print('OUTPUT: ', output)
-    print('PREDICTION: ', prediction)
+    print('PREDICTION: ', prediction, '\n')
+
+print('Average Error: ', sum / length)
+print(model.summary())
